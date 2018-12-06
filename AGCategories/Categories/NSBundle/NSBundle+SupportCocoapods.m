c42a1a2bd1d9c2dd45a9f621b8c24b14ee4fc3f1
JohnnyB0Y <JohnnyB0Y@MacBook-Pro.local>
2018-12-06 14:17:14 +0800

    完善

--- AGCategories/Categories/NSBundle/NSBundle+SupportCocoapods.m
+++ AGCategories/Categories/NSBundle/NSBundle+SupportCocoapods.m
@@ -13,15 +13,57 @@
 + (NSBundle *) ag_cocoapodsBundleForClass:(Class)aClass
 {
     NSBundle *bundle = [NSBundle bundleForClass:aClass];
-    NSURL *url = [bundle URLForResource:@"ResourceFramework" withExtension:@"bundle"];
-    
+    NSURL *url = [bundle URLForResource:@"MyLibrary" withExtension:@"bundle"];
     if ( url == nil ) {
-        url = [bundle URLForResource:@"MyLibrary" withExtension:@"bundle"];
+        url = [bundle URLForResource:@"ResourceFramework" withExtension:@"bundle"];
     }
+    if ( url ) {
+        bundle = [NSBundle bundleWithURL:url];
+    }
+    return bundle;
+}
+
++ (NSBundle *) ag_bundleForClass:(Class)aClass
+                    withResource:(NSString *)name
+{
+    return [self ag_bundleForClass:aClass
+                      withResource:name
+                     withExtension:@"bundle"];
+}
+
++ (NSBundle *) ag_bundleForClass:(Class)aClass
+                    withResource:(NSString *)name
+                   withExtension:(NSString *)ext
+{
+    return [self ag_bundleForClass:aClass
+                      withResource:name
+                     withExtension:ext
+                             error:nil];
+}
+
++ (NSBundle *)ag_bundleForClass:(Class)aClass
+                   withResource:(NSString *)name
+                  withExtension:(NSString *)ext
+                          error:(NSError **)error
+{
+    NSBundle *bundle = [NSBundle bundleForClass:aClass];
+    
+    if ( bundle == nil ) {
+        NSString *msg = [NSString stringWithFormat:@"Bundle for %@ not found.", NSStringFromClass(aClass)];
+        *error = [NSError errorWithDomain:NSCocoaErrorDomain code:404 userInfo:@{@"msg":msg}];
+        return bundle;
+    }
+    
+    NSURL *url = [bundle URLForResource:name withExtension:ext];
     
     if ( url ) {
         bundle = [NSBundle bundleWithURL:url];
     }
+    else {
+        NSString *msg = [NSString stringWithFormat:@"%@.%@ not found.", name, ext];
+        *error = [NSError errorWithDomain:NSCocoaErrorDomain code:404 userInfo:@{@"msg":msg}];
+    }
+    
     return bundle;
 }
 
